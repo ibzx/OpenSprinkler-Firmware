6c03fece9909148aaa3ddeba13682e25fb54a9dc
Kevin <k@none.com>
2023-01-06 15:58:32 -0800

    Fix build script

--- build.sh
+++ build.sh
@@ -15,11 +15,16 @@ if [ "$1" == "demo" ]; then
 	apt-get install -y libmosquitto-dev
 	echo "Compiling firmware..."
 	g++ -o OpenSprinkler -DDEMO -std=c++14 -m32 main.cpp OpenSprinkler.cpp program.cpp opensprinkler_server.cpp utils.cpp weather.cpp gpio.cpp etherport.cpp mqtt.cpp -lpthread -lmosquitto
-elif [ "$1" == "BPI" ]; then
+elif [ "$1" == "bpi" ]; then
 	echo "Installing required libraries..."
 	apt-get install -y libmosquitto-dev libgpiod-dev
 	echo "Compiling firmware..."
 	g++ -o OpenSprinkler -DBPI main.cpp OpenSprinkler.cpp program.cpp opensprinkler_server.cpp utils.cpp weather.cpp gpio.cpp etherport.cpp mqtt.cpp -lpthread -lmosquitto -lgpiod
+elif [ "$1" == "osbo" ]; then
+	echo "Installing required libraries..."
+	apt-get install -y libmosquitto-dev
+	echo "Compiling firmware..."
+	g++ -o OpenSprinkler -DOSBO main.cpp OpenSprinkler.cpp program.cpp opensprinkler_server.cpp utils.cpp weather.cpp gpio.cpp etherport.cpp mqtt.cpp -lpthread -lmosquitto
 else
 	echo "Installing required libraries..."
 	apt-get update
